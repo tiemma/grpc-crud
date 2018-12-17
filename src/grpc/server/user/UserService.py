dd936078ef469806687ab1c6168e5aa0ae02aac5
blank <bakareemmanuel98@outlook.com>
2018-12-17 08:11:19 +0100

    Added proper logger with handlers Implemented better environment variable handling Restructured protobuf dependency imports Edited dockerfile and pipfile regarding issues installing dependencies in alpine images

--- src/grpc/server/user/UserService.py
+++ src/grpc/server/user/UserService.py
@@ -1,18 +1,33 @@
+from os import getenv
+
 from bcrypt import hashpw, gensalt
 
 from time import sleep
 from uuid import uuid1, uuid4
 
+from logger import Logger
 
-from proto.helpers import create_timestamp, create_user_service
+from proto.helpers import create_timestamp, create_user_service, default_port
 from proto.User_pb2_grpc import CreateUserServiceServicer
 from proto.User_pb2 import User, UserResponse
 from proto.Helpers_pb2 import Timestamp, UUID, UserStatus
 
 
 class UserService(CreateUserServiceServicer):
+    """
+
+    """
+
+    logger = Logger.get_logger(__name__)
 
     def CreateUser(self, request, context):
+        """
+
+        :param request:
+        :param context:
+        :return:
+        """
+        self.logger.debug('Creating user response object')
         encr_password = hashpw(request.password.encode(), gensalt())
         user = User(name=request.name,
                     email=request.email,
@@ -23,6 +38,7 @@ class UserService(CreateUserServiceServicer):
         user_status = UserStatus.Value(UserStatus.Name(0))
         is_verified = False
         token = str(uuid4())
+        self.logger.debug('User response object successfully created')
         return UserResponse(user=user,
                             userStatus=user_status,
                             userId=user_id,
@@ -33,7 +49,9 @@ class UserService(CreateUserServiceServicer):
 
 
 def serve():
+    """
 
+    """
     # Sleep time in seconds
     one_day_in_seconds = 60 * 60 * 24
 
@@ -41,7 +59,7 @@ def serve():
 
     server.start()
 
-    print("Server started successfully")
+    Logger.get_logger(__file__).debug("Server started successfully on port {}".format(getenv('PORT', default_port)))
 
     try:
         while True:
