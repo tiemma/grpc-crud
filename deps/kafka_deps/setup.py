831727f4c59aebd7b23be4bad4839eec03682384
blank <bakareemmanuel98@outlook.com>
2018-12-21 22:34:04 +0100

    Adding in default package files for kafka_deps package

---
+++ deps/kafka_deps/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+__VERSION__ = '0.12'
+
+setup(name='kafka_deps',
+      version=__VERSION__,
+      description='Package containing code for publishing and consumin data [INTERNAL]',
+      url='https://github.com/Tiemma/grpc-crud',
+      author='Bakare Emmanuel',
+      author_email='emmanueltimmy98@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      package_data={'.': ['.env']},
+      include_package_data=True,
+      zip_safe=False)
