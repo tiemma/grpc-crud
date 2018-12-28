52818351a1e45f49736ba494fa4bca43727ee452
blank <bakareemmanuel98@outlook.com>
2018-12-28 08:53:59 +0100

    Moved service creation out of package Refactored variable naming Added better logging Made configurable options environment variables

--- src/grpc/server/user/UserService.py
+++ src/grpc/server/user/UserService.py
@@ -1,14 +1,17 @@
 from os import getenv
 
+
 from bcrypt import hashpw, gensalt
+from concurrent import futures
+from grpc import server as grpc_server, ssl_server_credentials
 
 from time import sleep
 from uuid import uuid1, uuid4
 
 from logger import Logger
 
-from proto.helpers import create_timestamp, create_user_service, default_port
-from proto.User_pb2_grpc import CreateUserServiceServicer
+from proto.helpers import create_timestamp, default_port, default_host_name, get_encoded_cert_key, get_encoded_cert
+from proto.User_pb2_grpc import CreateUserServiceServicer, add_CreateUserServiceServicer_to_server
 from proto.User_pb2 import User, UserResponse
 from proto.Helpers_pb2 import Timestamp, UUID, USER
 
@@ -22,12 +25,15 @@ class UserService(CreateUserServiceServicer):
 
     def CreateUser(self, request, context):
         """
+        CreateUser class overrides one in CreateUserServiceServicer which will throw a
+        NotImplementedError hence the reason for leaving it capitalized and now following
+        format highlighted in other functions below, for example
 
         :param request:
         :param context:
         :return:
         """
-        self.logger.debug('Creating user response object')
+        self.logger.debug('Creating user response object with request %r and context %r', request, context)
         encr_password = hashpw(request.password.encode(), gensalt())
         user = User(name=request.name,
                     email=request.email,
@@ -48,6 +54,22 @@ class UserService(CreateUserServiceServicer):
                             is_verified=is_verified)
 
 
+def create_user_service(server, user_service):
+    """
+
+    :param server: GRPC Server Instance
+    :param user_service: Instance of the rpc service defined in the protobuf struct
+    :return:
+    """
+    add_CreateUserServiceServicer_to_server(user_service, server)
+    server_creds = ssl_server_credentials(((get_encoded_cert_key(), get_encoded_cert(),),))
+    server.add_secure_port(
+        '{HOST}:{PORT}'.format(HOST=getenv('GRPC_HOST_NAME', default_host_name), PORT=getenv('GRPC_PORT', default_port)),
+        server_creds)
+
+    return server
+
+
 def serve():
     """
 
@@ -55,11 +77,13 @@ def serve():
     # Sleep time in seconds
     one_day_in_seconds = 60 * 60 * 24
 
-    server = create_user_service(UserService())
+    server = grpc_server(futures.ThreadPoolExecutor(max_workers=int(getenv('GRPC_MAX_WORKERS', '10'))))
+
+    create_user_service(server, UserService())
 
     server.start()
 
-    Logger.get_logger(__name__).debug("Server started successfully on port {}".format(getenv('PORT', default_port)))
+    Logger.get_logger(__name__).debug("Server started successfully on port {}".format(getenv('GRPC_PORT', default_port)))
 
     try:
         while True:
